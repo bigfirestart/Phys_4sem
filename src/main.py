d83c4e8ead2ffbf3596dba6767b54f7c6d86b4e5
Mark <bigfirestart@gmail.com>
2021-06-07 16:24:47 +0300

    feat: many calculates in one iteration

--- src/main.py
+++ src/main.py
@@ -1,15 +1,20 @@
-import matplotlib.pyplot as plt
-
 from src.brown_movement import BrownMovement
+from src.plot import Plot
 
 
 def lam2():
+    plot = Plot()
     x, y = BrownMovement(tmp=10, p=1e4, r=1e-6, N=250).act()
-    plt.plot(x, y, marker='o')
-    plt.grid(True)
-    plt.show()
+    plot.draw(x, y)
+    x, y = BrownMovement(tmp=293.15, p=1e4, r=1e-8, N=1000).act()
+    plot.draw(x, y)
+    x, y = BrownMovement(tmp=293.15, p=1e4, r=1e-8, N=250).act()
+    plot.draw(x, y)
+    x, y = BrownMovement(tmp=220.5, p=1e4, r=1e-6, N=250).act()
+    plot.draw(x, y)
+    x, y = BrownMovement(tmp=293.15, p=1e4, r=1e-11, N=250).act()
+    plot.draw(x, y)
 
 
 if __name__ == "__main__":
     lam2()
-
